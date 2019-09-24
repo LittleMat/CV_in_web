7ea39f5754e099b2fce95a3b796d7797d2b87c9c
mathieu <matmont98@gmail.com>
2019-09-24 08:18:56 +0200

    start of english version

---
+++ src/checker.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+int n;
+
+int main(int argc, char* argv[]) {
+	registerTestlibCmd(argc, argv);
+	n = inf.readInt();
+	readAndCHeckAnswer(ans)
+}
